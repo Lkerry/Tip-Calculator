a422c2775b881a0c424cfde933a3f7075408e899
Anne Kerrie LEVEILLE <annekerrieleveille@yahoo.fr>
2018-09-07 22:14:33 -0400

    The last Commit before submit
    Add gesture on plan text of the calculator

---
+++ Tip Calculator/SettingsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.swift
+//  Tip Calculator
+//
+//  Created by Kerry LEVEILLE on 9/2/18.
+//  Copyright © 2018 Kerry LEVEILLE. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: NSObject {
+
+}
